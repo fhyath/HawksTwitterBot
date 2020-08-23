8f620775af8e9c5e9b2800fb5d0acc7b1854f9e5
unknown <darkfang1231@gmail.com>
2020-08-23 00:52:43 -0400

    make it better

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import tweepy
 import time as timer
-from html.parser import HTMLParser
 from datetime import datetime, timedelta
 
 auth = tweepy.OAuthHandler('C733a1oBB5Ug9vty4XNDSFjTj', 'eaOE3Ao0RvIc2XPq2sTczc5TDCc0auqxVqCIktwI2bQUCTnQ4a')
@@ -11,20 +10,18 @@ user = api.me()
 
 # print(user.screen_name)
 
-d = datetime.today() - timedelta(hours=12)
+d = datetime.today() - timedelta(hours=3)
 
 def woj():
     woj_tweets = []
     for status in tweepy.Cursor(api.user_timeline, id="wojespn", tweet_mode="extended").items(100):
-        if ("Pacers" in status.full_text) or ("Atlanta's" in status.full_text) or ("Hawks" in status.full_text) or ("hawks" in status.full_text):
-            if ("@" in status.full_text):
+        if ("Atlanta" in status.full_text) or ("Atlanta's" in status.full_text) or ("Hawks" in status.full_text) or ("hawks" in status.full_text) or ("ATL" in status.full_text):
+            if (status.full_text.startswith('RT')):
                 continue
             if status.created_at >= d:
                 woj_tweets.append(status)
 
     for ww in woj_tweets:
-        # w = ww.full_text
-        # print(w)
         try:
             api.update_status("https://twitter.com/wojespn/status/" + str(ww.id))
         except tweepy.TweepError:
@@ -32,12 +29,6 @@ def woj():
             pass
 
 
-    # functions to run
-woj()
-
-    # testers for now, will remove after a beta deploy period
-    # c += 1
-    # api.update_status("TESTING HEROKU" + str(c))
-
-    # # wake up to potentially tweet only once every 3 hours
-    # timer.sleep(10800)
+while True:    
+    woj()
+    timer.sleep(10800)
